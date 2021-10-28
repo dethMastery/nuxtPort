33a6d5b97dd6389019e569af612feaedd11d2a66
Detzz <a2z.team.198@gmail.com>
2021-10-29 06:36:09 +0700

    rm mobile nav

--- components/Frame-Left.vue
+++ components/Frame-Left.vue
@@ -1,8 +1,5 @@
 <template>
     <b-col class="fwidth d-sm-none d-md-none d-lg-block d-none" cols="12" lg="3">
-        <b-col cols="12" class="mNav d-block d-lg-none d-xl-none d-md-block w-100">
-            <lFrame-Nav />
-        </b-col>
         <lFrame-lgNav />
     </b-col>
 </template>
