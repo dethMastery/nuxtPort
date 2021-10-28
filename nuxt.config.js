ebb7e8d398b720cc80fce6a035bdd1c5615939e2
Detzz <a2z.team.198@gmail.com>
2021-10-29 06:12:42 +0700

    change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nPort',
+    title: 'Suphakit P. | Portfolio',
     htmlAttrs: {
       lang: 'en'
     },
