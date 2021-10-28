121201133145149bbb53d2e6b63e3c720b684373
Detzz <a2z.team.198@gmail.com>
2021-10-29 05:10:10 +0700

    fix mobile nav

--- components/Frame.vue
+++ components/Frame.vue
@@ -2,13 +2,13 @@
     <b-container fluid>
         <b-row>
             <!-- Navbar -->
-                <b-col cols="12" class="mNav">
-                    <!-- <b-navbar toggleable="lg" type="dark" class="d-block d-lg-none d-xl-none d-md-block w-100">
-                        <b-navbar-brand href="#" class="w-80">Suphakit P.</b-navbar-brand>
+                <b-col cols="12" class="mNav d-block d-lg-none d-xl-none d-md-block w-100">
+                    <b-navbar toggleable="lg" type="dark">
+                        <b-navbar-brand href="#">Suphakit P.</b-navbar-brand>
 
-                        <b-navbar-toggle target="nav-collapse" class="w-20"></b-navbar-toggle>
+                        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-                        <b-collapse id="nav-collapse" is-nav>
+                        <b-collapse id="nav-collapse" style="text-align: center;" is-nav>
                             <b-navbar-nav>
                                 <b-nav-item href="/">
                                     Home
@@ -35,43 +35,6 @@
                                     Donation
                                 </b-nav-item>
                             </b-navbar-nav>
-                        </b-collapse>
-                    </b-navbar> -->
-                    
-                    <b-navbar toggleable="lg" type="dark" variant="info">
-                        <b-navbar-brand href="#">NavBar</b-navbar-brand>
-
-                        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-                        <b-collapse id="nav-collapse" is-nav>
-                        <b-navbar-nav>
-                            <b-nav-item href="#">Link</b-nav-item>
-                            <b-nav-item href="#" disabled>Disabled</b-nav-item>
-                        </b-navbar-nav>
-
-                        <!-- Right aligned nav items -->
-                        <b-navbar-nav class="ml-auto">
-                            <b-nav-form>
-                            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-                            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-                            </b-nav-form>
-
-                            <b-nav-item-dropdown text="Lang" right>
-                            <b-dropdown-item href="#">EN</b-dropdown-item>
-                            <b-dropdown-item href="#">ES</b-dropdown-item>
-                            <b-dropdown-item href="#">RU</b-dropdown-item>
-                            <b-dropdown-item href="#">FA</b-dropdown-item>
-                            </b-nav-item-dropdown>
-
-                            <b-nav-item-dropdown right>
-                            <!-- Using 'button-content' slot -->
-                            <template #button-content>
-                                <em>User</em>
-                            </template>
-                            <b-dropdown-item href="#">Profile</b-dropdown-item>
-                            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-                            </b-nav-item-dropdown>
-                        </b-navbar-nav>
                         </b-collapse>
                     </b-navbar>
 
