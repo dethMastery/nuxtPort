62275b3287fe03a9f4591119a5e332c86b49eb5d
Detzz <a2z.team.198@gmail.com>
2021-10-29 06:38:10 +0700

    edit mobile nav

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <b-container fluid>
+  <b-container fluid class="holder">
+    <b-col cols="12" class="mNav d-block d-lg-none d-xl-none d-md-block w-100">
+      <lFrame-Nav />
+    </b-col>
     <b-row>
       <Frame-Left />
       <Cont-Home />
@@ -9,6 +12,12 @@
 
 <style>
   @import url(./style/Frame.css);
+
+  .holder {
+    width: 100%;
+    margin: 0;
+    padding: 0;
+  }
 </style>
 
 <script lang="ts">
