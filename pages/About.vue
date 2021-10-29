5edc7766149b62694949a5a0759bf3dd4d9ccf51
Detzz <a2z.team.198@gmail.com>
2021-10-29 23:47:57 +0700

    add about part

--- pages/About.vue
+++ pages/About.vue
@@ -1,13 +1,23 @@
 <template>
-  <Frame />
+  <b-container fluid class="holder">
+    <b-row>
+      <b-col cols="12" class="mNav d-block d-lg-none d-xl-none d-md-block w-100">
+        <lFrame-Nav />
+      </b-col>
+      <Frame-Left />
+      <Cont-About />
+    </b-row>
+  </b-container>
 </template>
 
 <style>
   @import url(./style/Frame.css);
+  @import url(./style/About.css);
 </style>
 
 <script lang="ts">
   import Vue from 'vue'
+import ContAbout from '~/components/Cont-About.vue'
 
-  export default Vue.extend({})
+  export default Vue.extend({ components: { ContAbout } })
 </script>
