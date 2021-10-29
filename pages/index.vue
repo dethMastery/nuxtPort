1c02b2d88213a8b2f8e541848a7746976a2144ac
Detzz <a2z.team.198@gmail.com>
2021-10-29 20:55:20 +0700

    add compo

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <b-container fluid class="holder">
-    <b-col cols="12" class="mNav d-block d-lg-none d-xl-none d-md-block w-100">
-      <lFrame-Nav />
-    </b-col>
     <b-row>
+      <b-col cols="12" class="mNav d-block d-lg-none d-xl-none d-md-block w-100">
+        <lFrame-Nav />
+      </b-col>
       <Frame-Left />
       <Cont-Home />
     </b-row>
@@ -12,12 +12,7 @@
 
 <style>
   @import url(./style/Frame.css);
-
-  .holder {
-    width: 100%;
-    margin: 0;
-    padding: 0;
-  }
+  @import url(./style/Home.css);
 </style>
 
 <script lang="ts">
