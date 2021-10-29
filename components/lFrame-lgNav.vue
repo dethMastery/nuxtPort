af77dc234f81a362f88f39b01c6a821f026b5f42
Detzz <a2z.team.198@gmail.com>
2021-10-29 23:45:54 +0700

    config menu

--- components/lFrame-lgNav.vue
+++ components/lFrame-lgNav.vue
@@ -1,5 +1,5 @@
 <template>
-    <div style="padding-left: 15px;">
+    <div>
         <h1 class="lHeader" style="text-align: center;">
             Portfolio
         </h1>
@@ -14,43 +14,43 @@
             <br />
             <br />
             <b-nav vertical>
-                <b-nav-item href="/" class="lNavLink">
-                    <h5>
+                <b-nav-item href="/" class="lNavLink pt-1">
+                    <h5 style="padding-left: 15px;">
                         Home
                     </h5>
                 </b-nav-item>
-                <b-nav-item href="/About" class="lNavLink">
-                    <h5>
+                <b-nav-item href="/About" class="lNavLink pt-1">
+                    <h5 style="padding-left: 15px;">
                         About
                     </h5>
                 </b-nav-item>
-                <b-nav-item href="/Skill" class="lNavLink">
-                    <h5>
+                <b-nav-item href="/Skill" class="lNavLink pt-1">
+                    <h5 style="padding-left: 15px;">
                         Skill
                     </h5>
                 </b-nav-item>
-                <b-nav-item href="/Works" class="lNavLink">
-                    <h5>
+                <b-nav-item href="/Works" class="lNavLink pt-1">
+                    <h5 style="padding-left: 15px;">
                         Works
                     </h5>
                 </b-nav-item>
-                <b-nav-item href="/Cert" class="lNavLink">
-                    <h5>
+                <b-nav-item href="/Cert" class="lNavLink pt-1">
+                    <h5 style="padding-left: 15px;">
                         Certificates
                     </h5>
                 </b-nav-item>
-                <b-nav-item href="/Commission" class="lNavLink">
-                    <h5>
+                <b-nav-item href="/Commission" class="lNavLink pt-1">
+                    <h5 style="padding-left: 15px;">
                         Commission
                     </h5>
                 </b-nav-item>
-                <b-nav-item href="/Contact" class="lNavLink">
-                    <h5>
+                <b-nav-item href="/Contact" class="lNavLink pt-1">
+                    <h5 style="padding-left: 15px;">
                         Contact
                     </h5>
                 </b-nav-item>
-                <b-nav-item href="/Donation" class="lNavLink">
-                    <h5>
+                <b-nav-item href="/Donation" class="lNavLink pt-1">
+                    <h5 style="padding-left: 15px;">
                         Donation
                     </h5>
                 </b-nav-item>
