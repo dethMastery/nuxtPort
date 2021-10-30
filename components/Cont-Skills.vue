e7df07ffed9d4ac89c554bccd330d6d7293d2c3d
Detzz <a2z.team.198@gmail.com>
2021-10-30 21:54:09 +0700

    add skill part

--- components/Cont-Skills.vue
+++ components/Cont-Skills.vue
@@ -11,5 +11,73 @@
             </div>
         </div>
 
+        <!-- Skill Part -->
+        <div class="Skill">
+            <h5>
+                <b>
+                    Front-End Web Devolopment
+                </b>
+            </h5>
+            <b-progress :max="max">
+                <b-progress-bar class="html" :value="values[0]" animated show-progress>
+                    HTML
+                </b-progress-bar>
+                <b-progress-bar class="css" :value="values[1]" animated show-progress>
+                    CSS
+                </b-progress-bar>
+                <b-progress-bar class="jsF" :value="values[2]" animated show-progress>
+                    JS Framework
+                </b-progress-bar>
+            </b-progress>
+            <br/>
+            <h5>
+                <b>
+                    Back-End Web Development
+                </b>
+            </h5>
+            <b-progress :max="max">
+                <b-progress-bar class="php" :value="values[3]" animated show-progress>
+                    PHP
+                </b-progress-bar>
+                <b-progress-bar class="js" :value="values[4]" animated show-progress>
+                    JavaScript
+                </b-progress-bar>
+                <b-progress-bar class="ts" :value="values[5]" animated show-progress>
+                    TypeScript
+                </b-progress-bar>
+            </b-progress>
+            <br>
+
+            <h5>
+                <b>
+                    Music Production
+                </b>
+            </h5>
+            <b-progress :max="max">
+                <b-progress-bar class="master" :value="values[6]" animated show-progress>
+                    Mastering
+                </b-progress-bar>
+                <b-progress-bar class="sing" :value="values[7]" variant="info" animated show-progress>
+                    Singing
+                </b-progress-bar>
+                <b-progress-bar class="prod" :value="values[8]" animated show-progress>
+                    Producing
+                </b-progress-bar>
+                <b-progress-bar class="comp" :value="values[9]" animated show-progress>
+                    Composing
+                </b-progress-bar>
+            </b-progress>
+        </div>
     </b-col>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        values: [25,40,35, 40,35,25, 10,20,40,30],
+        max: 100
+      }
+    }
+  }
+</script>
