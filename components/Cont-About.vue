0e985b3f060127b8bb575557b8c3e3b544fcfa75
Detzz <a2z.team.198@gmail.com>
2021-10-30 14:05:19 +0700

    add header

--- components/Cont-About.vue
+++ components/Cont-About.vue
@@ -1,5 +1,13 @@
 <template>
     <b-col cols="12" lg="9" class="rPart" style="padding: 0!important;">
-        
+        <div class="img-head">
+            <div class="bs">
+                <div class="head-text">
+                    <h1>
+                        About
+                    </h1>
+                </div>
+            </div>
+        </div>
     </b-col>
 </template>
