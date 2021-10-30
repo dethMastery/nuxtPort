87090baab29ac455db62036776fc3cc43ae488c3
Detzz <a2z.team.198@gmail.com>
2021-10-30 17:37:30 +0700

    adding header path

--- components/Cont-Skills.vue
+++ components/Cont-Skills.vue
@@ -1,3 +1,15 @@
 <template>
-    
+    <b-col cols="12" lg="9" class="rPart" style="padding: 0!important;">
+        <!-- Header IMG -->
+        <div class="img-head">
+            <div class="bs">
+                <div class="head-text">
+                    <h1>
+                        Skills
+                    </h1>
+                </div>
+            </div>
+        </div>
+
+    </b-col>
 </template>
