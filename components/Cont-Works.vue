ab6e937c660c86385776dc5d0f52e2305e22a0ea
Detzz <a2z.team.198@gmail.com>
2021-11-01 07:56:38 +0700

    add modal

--- components/Cont-Works.vue
+++ components/Cont-Works.vue
@@ -13,10 +13,10 @@
 
         <!-- Works Part -->
         <div class="works">
-            <b-row style="width: 100%;! important">
+            <b-row style="width: 100%; !important">
                 <div v-for="info in card" :key="info.id" class="col-12 col-md-6 pr-1 pb-2">
                     <div class="card h-100">
-                        <a href="#working" class="card-img-wrap" data-bs-toggle="modal" data-bs-target="#working-2-b">
+                        <a href="#" class="card-img-wrap" v-b-modal.{{info.id}}>
                             <img class="card-img-top" v-bind:src="info.pix">
                         </a>
                         <div class="card-body">
@@ -34,6 +34,22 @@
                     </div>
                 </div>
             </b-row>
+
+            <div v-for="info in card" :key="info.id">
+                <section class="modal fade" v-bind:id="info.id" tabindex="-1" :aria-labelledby="info.id" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <span> {{info.name}} </span>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" style="color: white;"></button>
+                    </div>
+                    <div class="modal-body">
+                        <img src="asset/image/working/working-4.webp" alt="cert-1" class="modal-img">
+                    </div>
+                    </div>
+                </div>
+                </section>
+            </div>
         </div>
     </b-col>
 </template>
