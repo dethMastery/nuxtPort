943122f09bf5cd5b54f5ab9b5b53f6af4c62aba4
Detzz <a2z.team.198@gmail.com>
2021-11-03 06:47:02 +0700

    fix text

--- components/lFrame-lgNav.vue
+++ components/lFrame-lgNav.vue
@@ -36,7 +36,7 @@
                 </b-nav-item>
                 <b-nav-item href="/Cert" class="lNavLink pt-1">
                     <h5 style="padding-left: 15px;">
-                        Certificates
+                        Certificate
                     </h5>
                 </b-nav-item>
                 <b-nav-item href="/Commission" class="lNavLink pt-1">
