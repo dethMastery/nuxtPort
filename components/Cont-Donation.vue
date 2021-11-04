c829552368a8fc556217cb08ea00faccc3a7f59f
Detzz <a2z.team.198@gmail.com>
2021-11-05 03:50:47 +0700

    setup donation page

---
+++ components/Cont-Donation.vue
@@ -0,0 +1,19 @@
+<template>
+    <b-col cols="12" lg="9" class="rPart" style="padding: 0!important;">
+        <!-- Heading IMG -->
+        <div class="img-head">
+            <div class="bs">
+                <div class="head-text">
+                    <h1>
+                        Donation
+                    </h1>
+                </div>
+            </div>
+        </div>
+
+        <!-- Info Part -->
+        <div class="Donation">
+            
+        </div>
+    </b-col>
+</template>
